bfa7588ac95867c128cc86ec73d8756c6b76c46c
Tobbe821 <34543773+Tobbe821@users.noreply.github.com>
2022-12-10 10:20:13 +0100

    ref

--- lab_2/knn.py
+++ lab_2/knn.py
@@ -1,123 +1,80 @@
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import datasets
 import pandas as pd
-from sklearn.inspection import DecisionBoundaryDisplay
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import sklearn.metrics
-from matplotlib.colors import ListedColormap
-import sklearn.preprocessing as skpre
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.metrics import accuracy_score
+import knn_draw
+
+FILES = ['lab_2/1-titanic-small.csv', 'lab_2/2-titanic2attr.csv', 'lab_2/3-titanic.csv', 'lab_2/4-test.csv']
 
 def load_csv(path):
     """Loads csv file"""
     dataFrame = pd.read_csv(path, sep=",", encoding="utf-8")
     return dataFrame
 
-def knn(dataFrame):
-    """K nearest neighbour"""
-    X = dataFrame.iloc[:,2:4]
-    y = dataFrame.iloc[:,1]
-    
-    knn = KNeighborsClassifier(n_neighbors=3)
-    knn.fit(X,y)
-    
-    op = knn.predict(X)
-    acc = sklearn.metrics.accuracy_score(y, op)
-    
-    dataFrame_2 = load_csv("lab_2/2-titanic2attr.csv")
-    dataFrame_2 = dataFrame_2.dropna()
-    
-    X_2 = dataFrame_2.iloc[:,2:4]
-    y_2 = dataFrame_2.iloc[:,1]
-    
-    knn_2 = KNeighborsClassifier(n_neighbors=3)
-    knn_2.fit(X_2,y_2)
-    
-    op_2 = knn_2.predict(X_2)
-    acc_2 = sklearn.metrics.accuracy_score(y_2,op_2)
-    # print(acc_2)
-    # draw(dataFrame, X_2, y_2, knn)
-    
+def knn_model(X, y, dataFrame):
+    model = KNeighborsClassifier(n_neighbors=3)
+    model.fit(X,y)
+    pred = model.predict(X)
+    try:
+        knn_draw.draw(dataFrame, X, y, model)
+    except:
+        print("failed to draw")
+    return model, accuracy_score(y, pred)
 
-    dataFrame_3 = load_csv("lab_2/3-titanic.csv")
+def knn():
+    """K nearest neighbour"""
+    # Titanic-small
+    dataFrame = load_csv(FILES[0])
+    print(knn_model(dataFrame.iloc[:,2:4], dataFrame.iloc[:,1], dataFrame)[1])
+    
+    # Titanic-test
+    dataFrame_4 = load_csv(FILES[3])
+    dataFrame_4 = dataFrame_4.dropna()
+    print(knn_model(dataFrame_4.iloc[:,2:4], dataFrame_4.iloc[:,1], dataFrame_4)[1])
+    
+    # Titanic-2
+    dataFrame_2 = load_csv(FILES[1])
+    dataFrame_2 = dataFrame_2.dropna()
+    print(knn_model(dataFrame_2.iloc[:,2:4], dataFrame_2.iloc[:,1], dataFrame)[1])
+
+    # Titanic-3 full
+    dataFrame_3 = load_csv(FILES[2])
     dataFrame_3 = dataFrame_3.dropna()
     
     labels = ['Ticket', 'Sex', 'Embarked', 'Fare']
-    le = skpre.LabelEncoder()
+    le = LabelEncoder()
     for l in labels:
         dataFrame_3[l] = le.fit_transform(dataFrame_3[l])
+    
+    print(knn_model(dataFrame_3.iloc[:,2:10], dataFrame_3.iloc[:,1], dataFrame)[1])
+    
 
-    X_3 = dataFrame_3.iloc[:,2:10]
-    y_3 = dataFrame_3.iloc[:,1]
-    
-    print(X_3)
-    
-    knn_3 = KNeighborsClassifier(n_neighbors=3)
-    knn_3.fit(X_3,y_3)
-    
-    op_3 = knn_3.predict(X_3)
-    acc_3 = sklearn.metrics.accuracy_score(y_3, op_3)
-    
-    print(acc_3)
-    #draw(dataFrame_3, X_3, y_3, knn_3)
-    
-def upg2():
-    df = load_csv("lab_2/3-titanic.csv")
-    
+def dataFrame_properties():
+    dataFrame = load_csv(FILES[2])
     labels = ['Ticket', 'Sex', 'Embarked', 'Fare']
-    le = skpre.LabelEncoder()
+    le = LabelEncoder()
     for l in labels:
-        df[l] = le.fit_transform(df[l])
+        dataFrame[l] = le.fit_transform(dataFrame[l])
     
-    # print(df.shape)
-    # print(df.dtypes)
-    print(df.describe())
-    # print(df['Sex'].value_counts())
-    
-    df_fem  = df[df['Sex']==0]
-    df_male  = df[df['Sex']==1]
-    
-    print(df_fem['Survived'].value_counts())
-    print(df_male['Survived'].value_counts())
-    
-    print(df_fem.describe())
-    print(df_male.describe())
-    
-def draw(dataFrame, X, y, knn):
-    cmap_light = ListedColormap(["orange", "cyan"])
-    cmap_bold = ["darkorange", "c"]
+    print("Shape \n", dataFrame.shape, "\n")
+    print("dtypes \n", dataFrame.dtypes, "\n")
 
-    _, ax = plt.subplots()
-    DecisionBoundaryDisplay.from_estimator(
-        knn, X, cmap=cmap_light,
-        ax=ax, response_method = "predict",
-        plot_method="pcolormesh",
-        xlabel=dataFrame.columns[2],
-        ylabel=dataFrame.columns[3],
-        shading="auto"
-    )
+    df_fem  = dataFrame[dataFrame['Sex']==0]
+    df_male  = dataFrame[dataFrame['Sex']==1]
     
-    sns.scatterplot(
-        x=X.iloc[:, 0],
-        y=X.iloc[:, 1],
-        hue=y,
-        palette=cmap_bold,
-        alpha=1.0,
-        edgecolor="black",
-    )
-   
-    plt.title(
-        "Classification"
-    )
-    plt.show()
+    print("Female passengers: ", len(df_fem))
+    print("Survived: ", df_fem['Survived'].sum())
+    print("Percentage: ", df_fem['Survived'].sum()/len(df_fem), "\n")
+    
+    print("Male passengers: ", len(df_male))
+    print("Survived: ", df_male['Survived'].sum())
+    print("Percentage: ", df_male['Survived'].sum()/len(df_male), "\n")
+
 
 def main():
     """Main function"""
-    #dataFrame = load_csv("lab_2/1-titanic-small.csv")
-    #knn(dataFrame)
-    upg2()
-
+    knn()
+    dataFrame_properties()
 
 if __name__ == '__main__':
     main()
