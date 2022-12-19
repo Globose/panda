de4c81a6031dced6217279df3ea0888ffa3256d9
Tobbe821 <34543773+Tobbe821@users.noreply.github.com>
2022-12-19 22:32:50 +0100

    upd

--- lab_2/knn.py
+++ lab_2/knn.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -55,24 +55,40 @@ def classification():
     for l in labels:
         dataFrame[l] = le.fit_transform(dataFrame[l])
     
-    X = dataFrame.iloc[:,2:10]
+    X = dataFrame.iloc[:,[2,3,4,5,6,7,8,9]]
     y = dataFrame.iloc[:,1]
 
-    data = train_test_split(X,y,train_size=0.7, test_size=0.3, random_state=0)
+    data = train_test_split(X,y,train_size=0.7, test_size=0.3)
     dtree = DecisionTreeClassifier()
     dtree.fit(data[0],data[2])
     prediction = dtree.predict(data[1])
-    print("Dtree: ", accuracy_score(data[3], prediction))
-    #generate_color_image(dtree, X)
     
-    random_forest = RandomForestClassifier(n_estimators=1000)
+    hm = {}
+    hm["dtree_acc"] = accuracy_score(data[3],prediction)
+    hm["dtree_pre"] = precision_score(data[3],prediction)
+    hm["dtree_f1"] = f1_score(data[3],prediction)
+    hm["dtree_rec"] = recall_score(data[3],prediction)
+    
+    random_forest = RandomForestClassifier()
     random_forest.fit(data[0], data[2])
     prediction = random_forest.predict(data[1])
-    print("Rforest: ", accuracy_score(data[3], prediction))
     
+    hm["rfor_acc"] = accuracy_score(data[3],prediction)
+    hm["rfor_pre"] = precision_score(data[3],prediction)
+    hm["rfor_f1"] = f1_score(data[3],prediction)
+    hm["rfor_rec"] = recall_score(data[3],prediction)
+    
+    model = KNeighborsClassifier(n_neighbors=3)
+    model.fit(X,y)
+    prediction = model.predict(X)
+    hm["knn_acc"] = accuracy_score(data[3],prediction)
+    hm["knn_pre"] = precision_score(data[3],prediction)
+    hm["knn_f1"] = f1_score(data[3],prediction)
+    hm["knn_rec"] = recall_score(data[3],prediction)
+    return hm
 
 def dataFrame_properties():
-    dataFrame = load_csv(FILES[2])
+    dataFrame = load_csv(FILES[2]).dropna()
     labels = ['Ticket', 'Sex', 'Embarked', 'Fare']
     le = LabelEncoder()
     for l in labels:
@@ -95,10 +111,20 @@ def dataFrame_properties():
 
 def main():
     """Main function"""
-    knn()
+    # knn()
     # dataFrame_properties()
-    # decision_tree()
     # classification()
+    results = {}
+    for z in range(40):
+        hm = classification()
+        for m in hm:
+            if results.get(m) is None:
+                results[m] = [hm.get(m)]
+            else:
+                results[m].append(hm.get(m))
+    for key, lst in results.items():
+        print(key, sum(lst)/len(lst))
+            
 
 if __name__ == '__main__':
     main()
